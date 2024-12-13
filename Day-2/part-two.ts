c2562e8913d45550bdc8367f686ea50111d699c6
Adjanour <adjanour@icloud.com>
2024-12-13 09:07:07 +0000

    Day2 part-2

---
+++ Day-2/part-two.ts
@@ -0,0 +1,46 @@
+
+const data = await Deno.readTextFile("input.txt");
+
+const lines = data.split("\n").filter((line: string) => line.trim() !== "");
+
+const Reports: number[][] = lines.map((line: string) => {
+  return line.split(" ")
+    .map((char: string) => parseInt(char, 10))
+    .filter(Number.isFinite); // Exclude NaN values
+});
+
+const validateReport = (report: number[]): boolean => {
+  if (report.length < 2) return false;
+
+  const initialTrend = report[1] - report[0];
+
+  for (let i = 0; i < report.length - 1; i++) {
+    const difference = Math.abs(report[i] - report[i + 1]);
+    const currentTrend = report[i + 1] - report[i];
+
+    if (difference > 3) return true;              // Check if the difference exceeds 3
+    if (report[i] === report[i + 1]) return true; // Check if two consecutive numbers are equal
+    if ((initialTrend > 0 && currentTrend < 0) || (initialTrend < 0 && currentTrend > 0)) {
+      return true; // Check for trend reversal
+    }
+  }
+
+  return false;
+};
+
+const validateWithDamper = (report: number[]) => {
+  if (!validateReport(report)) return false;
+
+  for (let i = 0; i < report.length; i++) {
+    const modifiedReport = [...report.slice(0, i), ...report.slice(i + 1)]
+    if (!validateReport(modifiedReport)) return false;
+  }
+  return true;
+}
+const safeReports = Reports.filter((report: number[]) => !validateWithDamper(report));
+
+const numberOfSafeReports = safeReports.length;
+
+console.table(safeReports);
+console.log("Number of safe reports:", numberOfSafeReports);
+console.log("Total reports:", Reports.length);
